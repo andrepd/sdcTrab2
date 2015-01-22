909351b945fb2df9c8ee458a2a2aa5a410475563
andrepd <andrepduarte@gmail.com>
2015-01-22 17:51:28 +0000

    patch

--- main.cpp
+++ main.cpp
@@ -18,31 +18,32 @@
 // Begin user area
 
 // Time interval and step
-const double tf=2000,h=0.004;
+const long double tf=2000,h=0.008;
 
 // Number of dependent variables
 const int N=3;
 
 // Initial values
-double init[]={2,0,0};
+long double init[]={2,0,0};
 
 // User constants
-const double s = 10;
-const double b = 8/3;
-double r = 24.4;
+const long double s = 10;
+const long double b = 8/3;
+long double r = 24.4;
 
 // Function definitions
 //   d/dt (x[n]) = fn
-double f1(std::valarray<double> xx) {
-    return s*(xx[1]-xx[0]);
+long double f1(std::valarray<long double> x) {
+    return s*(x[1]-x[0]);
 }
 
-double f2(std::valarray<double> xx) {
-    return (r-xx[2])*xx[0]-xx[1];
+long double f2(std::valarray<long double> x) {
+	//return r*xx[0]-xx[1]-xx[0]*x[2];
+    return (r-x[2])*x[0]-x[1];
 }
 
-double f3(std::valarray<double> xx) {
-    return xx[0]*xx[1]-b*xx[2];
+long double f3(std::valarray<long double> x) {
+    return x[0]*x[1]-b*x[2];
 }
 
 // End user area
@@ -50,19 +51,19 @@ double f3(std::valarray<double> xx) {
 
 
 int main(int argc, char** argv) {
-	std::valarray<double> x(init,N);
+	std::valarray<long double> x(init,N);
 
-	typedef double (*f_T)(std::valarray<double>);
+	typedef long double (*f_T)(std::valarray<long double>);
 	std::vector<f_T> f {f1,f2,f3};
 
-    std::valarray<double> k1 (0.,N);
-    std::valarray<double> k2 (0.,N);
-    std::valarray<double> k3 (0.,N);
-    std::valarray<double> k4 (0.,N);
+	std::valarray<long double> k1 (0.,N);
+    std::valarray<long double> k2 (0.,N);
+    std::valarray<long double> k3 (0.,N);
+    std::valarray<long double> k4 (0.,N);
 
-    std::valarray<double> a (0.,N);
-	
-    for (double t=0;t<tf;t+=h) {
+    std::valarray<long double> a (0.,N);
+
+    for (long double t=0;t<tf;t+=h) {
 		r=24.4+sin(0.1*t);
         for(int i=0;i<N;i++) {
             k1[i] = f[i](x);
@@ -87,14 +88,13 @@ int main(int argc, char** argv) {
             x[i] += h / 6 * (k1[i] + 2 * k2[i] + 2 * k3[i] + k4[i]);
         }
 
-        std::printf("%f ",t);
+        std::printf("%Lf ",t);
 		for(int i=0;i<N-1;i++) {
-            std::printf("%f ", x[i]);
+            std::printf("%Lf ", x[i]);
         }
-        std::printf("%f\n",x[N]);
+        std::printf("%Lf\n",x[N-1]);
 
     }
 
     return 0;
 }
-
